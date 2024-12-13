7869c5c7f573865cac6af0d6b194050e54a7d9b3
vinypalazzi <vparreira10@gmail.com>
2024-12-13 00:54:45 -0300

    Add comentários in "Adivinhar" and "NumerosPrimos"

--- exercicios/exercicios/estruturas/Adivinhar.java
+++ exercicios/exercicios/estruturas/Adivinhar.java
@@ -9,6 +9,7 @@ public class Adivinhar {
 
         int num;
 
+        //Cria um número aleatório de 0 a 1, é multiplicado por 100 e transformado em inteiro
         int random = (int) (Math.random() * 100);
 
         int tentativas = 10;
@@ -16,6 +17,16 @@ public class Adivinhar {
         int totalTentativas = 0;
 
         System.out.println("\nJogo da adivinhação\nRegras\n- Será escolhido um número de 1 a 100\n- Você terá 10 chances para descobrir\n- Cada nova jogada você irá saber se é mais ou menos que seu valor escolhido\n");
+
+        /*
+        Loop que mostra as tentativas
+        pega um input do usuário
+        acrescenta o total de tentativas
+        verifica se está correto o input informado pelo usuário
+        caso seja correto, ele mostra o número de tentativas feitas e o número sorteado
+        diminui uma tentativa do usuário
+        e caso nenhum número estiver correto ele mostra qual era o número
+        */
 
         for (int i = 0; i <= 10; i++) {
             System.out.println("\nVocê ainda tem " + tentativas + " tentativas!");
--- exercicios/exercicios/estruturas/NumerosPrimos.java
+++ exercicios/exercicios/estruturas/NumerosPrimos.java
@@ -9,11 +9,25 @@ public class NumerosPrimos {
 
         System.out.println("\n-----------------------------\nVerificador de números primos\n-----------------------------\n");
 
+        //Pega um input do usuário
         System.out.print("Digite um número:\n- ");
         int num = scanner.nextInt();
 
         int cont = 0;
 
+        /*
+        Começa com 2 (Qualquer número é divisível por 1)
+        verifico se o resto da divisão entre i e o número escolhido é igual a 0
+        caso seja quer dizer que ele conseguiu dividir por outros números além de 1 e ele mesmo
+        sendo assim não sendo um número primo
+        caso isso aconteça ele já para o programa
+
+        caso não aconteça ele mostra que o número escolhido é primo
+
+        faço isso através de um contador fora do bloco 'for'
+        que se transforma em 2 caso não consiga dividir por nenhum número
+        e se transforma em 1 e encerra o programa caso consiga dividir
+        */
         for (int i = 2; i < num; i++) {
 
             int verificar = num % i;
