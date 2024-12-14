f270296b47fe384dda57d14d2edaeef4a5286e0c
vinypalazzi <vparreira10@gmail.com>
2024-12-14 02:03:50 -0300

    Ajustando Bug

--- exercicios/exercicios/java/estruturas/JogoSoma.java
+++ exercicios/exercicios/java/estruturas/JogoSoma.java
@@ -11,23 +11,28 @@ public class JogoSoma {
 
         double num = 0;
 
-        double numTemp;
+        double numTemp = 0;
 
         int cont = 0;
 
-        while (num >= 0) {
+        while (numTemp >= 0) {
 
             System.out.print("Digite um número: \n- ");
             numTemp = scanner.nextDouble();
 
-            num += numTemp;
+            if (numTemp > 0) {
+                num += numTemp;
+                cont += 1;
+            }
 
-            cont += 1;
 
-            if (numTemp < 0) {
-                System.out.println("\nO total entre os " + cont + " número(s) digitados foi " + num + "\n");
+            if (numTemp < 0 && cont > 1) {
+                System.out.println(
+                        "\n------------------------------------------------\n" +
+                        "O total entre os " + cont + " número(s) digitados foi " + num +
+                        "\n------------------------------------------------");
                 break;
-            } else if (cont > 1) {
+            } else if (cont >= 2) {
                 System.out.println("\nA soma entre os " + cont + " número(s) digitados é igual á " + num + "\n");
             }
         }
