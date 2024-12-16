e8a67231b97e5c50c6229ea28bcb5cb863e3667f
vinypalazzi <vparreira10@gmail.com>
2024-12-16 15:40:03 -0300

    Exercicio DATA adicionado

---
+++ exercicios/exercicios/java/classes/Data.java
@@ -0,0 +1,8 @@
+package exercicios.java.classes;
+
+// Classe para representar uma data com dia, mês e ano
+public class Data {
+    int dia; // Atributo para armazenar o dia
+    int mes; // Atributo para armazenar o mês
+    int ano; // Atributo para armazenar o ano
+}
---
+++ exercicios/exercicios/java/classes/DataTeste.java
@@ -0,0 +1,24 @@
+package exercicios.java.classes;
+
+// Classe para testar a classe Data
+public class DataTeste {
+    public static void main(String[] args) {
+        // Cria um objeto da classe Data e inicializa seus atributos
+        Data data1;
+        data1 = new Data();
+        data1.ano = 2024; // Define o ano de data1
+        data1.mes = 12;   // Define o mês de data1
+        data1.dia = 16;   // Define o dia de data1
+
+        // Cria outro objeto da classe Data e inicializa seus atributos
+        Data data2;
+        data2 = new Data();
+        data2.ano = 2008; // Define o ano de data2
+        data2.mes = 2;    // Define o mês de data2
+        data2.dia = 27;   // Define o dia de data2
+
+        // Exibe os anos dos dois objetos data no console
+        System.out.println(data1.ano); // Exibe o ano de data1
+        System.out.println(data2.ano); // Exibe o ano de data2
+    }
+}
